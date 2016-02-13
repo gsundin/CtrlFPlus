8b57d9ad21261ad3cddab4b91f25eb9595d99b9f
Hidenori Shinohara <shino013@umn.edu>
2016-02-13 17:15:18 -0600

    fix indentations

--- content_script.js
+++ content_script.js
@@ -2,51 +2,51 @@
 window.onload = function() {
 
   chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-      // var searchTerm = document.getElementById("input_text").value;
-      matchText(document, new RegExp("\\b" + request.searchTerm + "\\b", "g"), function(node, match, offset) {
-          var span = document.createElement("span");
-          span.style = "background-color:#FFFF01";
-          span.id = "search-highlight-text";
-          span.textContent = match;
-          return span;
-      });
-      sendResponse({messageStatus: "received"});
+    // var searchTerm = document.getElementById("input_text").value;
+    matchText(document, new RegExp("\\b" + request.searchTerm + "\\b", "g"), function(node, match, offset) {
+      var span = document.createElement("span");
+      span.style = "background-color:#FFFF01";
+      span.id = "search-highlight-text";
+      span.textContent = match;
+      return span;
+    });
+    sendResponse({messageStatus: "received"});
   });
 
 
   var matchText = function(node, regex, callback, excludeElements) { 
 
-      excludeElements || (excludeElements = ['script', 'style', 'iframe', 'canvas']);
-      var child = node.firstChild;
+    excludeElements || (excludeElements = ['script', 'style', 'iframe', 'canvas']);
+    var child = node.firstChild;
 
-      while (child) {
-          switch (child.nodeType) {
-          case 1:
-              if (child.id == "search-highlight-text") {
-                child.style = "";
-              }
-              if (excludeElements.indexOf(child.tagName.toLowerCase()) > -1)
-                  break;
-              matchText(child, regex, callback, excludeElements);
-              break;
-          case 3:
-              var bk = 0;
-              child.data.replace(regex, function(all) {
-                  var args = [].slice.call(arguments),
-                      offset = args[args.length - 2],
-                      newTextNode = child.splitText(offset+bk), tag;
-                  bk -= child.data.length + all.length;
+    while (child) {
+      switch (child.nodeType) {
+      case 1:
+        if (child.id == "search-highlight-text") {
+          child.style = "";
+        }
+        if (excludeElements.indexOf(child.tagName.toLowerCase()) > -1)
+          break;
+        matchText(child, regex, callback, excludeElements);
+        break;
+      case 3:
+        var bk = 0;
+        child.data.replace(regex, function(all) {
+          var args = [].slice.call(arguments),
+            offset = args[args.length - 2],
+            newTextNode = child.splitText(offset+bk), tag;
+          bk -= child.data.length + all.length;
 
-                  newTextNode.data = newTextNode.data.substr(all.length);
-                  tag = callback.apply(window, [child].concat(args));
-                  child.parentNode.insertBefore(tag, newTextNode);
-                  child = newTextNode;
-              });
-              break;
-          }
-          child = child.nextSibling;
+          newTextNode.data = newTextNode.data.substr(all.length);
+          tag = callback.apply(window, [child].concat(args));
+          child.parentNode.insertBefore(tag, newTextNode);
+          child = newTextNode;
+        });
+        break;
       }
+      child = child.nextSibling;
+    }
 
-      return node;
+    return node;
   };
 }
