48c48e9c7615f8f92858fc677297e53c458a764f
Hidenori Shinohara <shino013@umn.edu>
2016-02-13 11:19:48 -0600

    Now reaches content_script.js

--- content_script.js
+++ content_script.js
@@ -1,19 +1,25 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (true) {
-    highlightText(document.body);
-    sendResponse({messageStatus: "received"});
-  }
-});
+document.body.style.background = 'red'
 
-function highlightText(element) {
-  var nodes = element.childNodes;
-  for (var i = 0, l = nodes.length; i < l; i++) {
-    if (nodes[i].nodeType === 3)  // Node Type 3 is a text node
-      var text = nodes[i].innerHTML;
-      nodes[i].innerHTML = "<span style='background-color:#FFEA0'>" + text + "</span>";
-    }
-    else if (nodes[i].childNodes.length > 0) {
-      highlightText(nodes[i]);  // Not a text node or leaf, so check it's children
-    }
-  }
-}
+/*
+ * Hide
+ * The following is commented out temporarily
+ */
+//chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//  if (true) {
+//    highlightText(document.body);
+//    sendResponse({messageStatus: "received"});
+//  }
+//});
+//
+//function highlightText(element) {
+//  var nodes = element.childNodes;
+//  for (var i = 0, l = nodes.length; i < l; i++) {
+//    if (nodes[i].nodeType === 3) {  // Node Type 3 is a text node
+//      var text = nodes[i].innerHTML;
+//      nodes[i].innerHTML = "<span style='background-color:#FFEA0'>" + text + "</span>";
+//    }
+//    else if (nodes[i].childNodes.length > 0) {
+//      highlightText(nodes[i]);  // Not a text node or leaf, so check it's children
+//    }
+//  }
+//}
