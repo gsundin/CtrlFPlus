e57a2d84f6325b3e80372fb2abee66c3fee82773
Hidenori Shinohara <shino013@umn.edu>
2016-02-13 16:40:52 -0600

    kind of almost working ish

--- content_script.js
+++ content_script.js
@@ -1,71 +1,48 @@
-//document.body.style.background = 'blue'
 
-/*
- * Hide
- * The following is commented out temporarily
- */
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  // we reached here
-//  inputText = document.getElementById("inputText")
-//  var text = "ab";
-//  var inputText = document.body;
-//  var innerHTML = inputText.innerHTML;
-//  var index = innerHTML.indexOf(text);
-//  console.log("hello this is Hidenori");
-//  console.log("index is");
-//  console.log(index);
-//  if ( index >= 0 )
-//  { 
-//      innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
-//      inputText.innerHTML = innerHTML 
-//  }
+window.onload = function() {
 
-  if (true) {
-    // highlightText(document.body);
-    var searchTerm = "Example";
-;
-  matchText(document, new RegExp("\\b" + searchTerm + "\\b", "g"), function(node, match, offset) {
-      var span = document.createElement("span");
-      span.style = "background-color:#FFFF01";
-      span.textContent = match;
-      return span;
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+      var searchTerm = document.getElementById("input_text").value;
+      matchText(document, new RegExp("\\b" + searchTerm + "\\b", "g"), function(node, match, offset) {
+          var span = document.createElement("span");
+          span.style = "background-color:#FFFF01";
+          span.textContent = match;
+          return span;
+      });
+      sendResponse({messageStatus: "received"});
   });
 
 
-    sendResponse({messageStatus: "received"});
-  }
-});
+  var matchText = function(node, regex, callback, excludeElements) { 
 
+      excludeElements || (excludeElements = ['script', 'style', 'iframe', 'canvas']);
+      var child = node.firstChild;
 
-var matchText = function(node, regex, callback, excludeElements) { 
+      while (child) {
+          switch (child.nodeType) {
+          case 1:
+              if (excludeElements.indexOf(child.tagName.toLowerCase()) > -1)
+                  break;
+              matchText(child, regex, callback, excludeElements);
+              break;
+          case 3:
+              var bk = 0;
+              child.data.replace(regex, function(all) {
+                  var args = [].slice.call(arguments),
+                      offset = args[args.length - 2],
+                      newTextNode = child.splitText(offset+bk), tag;
+                  bk -= child.data.length + all.length;
 
-    excludeElements || (excludeElements = ['script', 'style', 'iframe', 'canvas']);
-    var child = node.firstChild;
+                  newTextNode.data = newTextNode.data.substr(all.length);
+                  tag = callback.apply(window, [child].concat(args));
+                  child.parentNode.insertBefore(tag, newTextNode);
+                  child = newTextNode;
+              });
+              break;
+          }
+          child = child.nextSibling;
+      }
 
-    while (child) {
-        switch (child.nodeType) {
-        case 1:
-            if (excludeElements.indexOf(child.tagName.toLowerCase()) > -1)
-                break;
-            matchText(child, regex, callback, excludeElements);
-            break;
-        case 3:
-            var bk = 0;
-            child.data.replace(regex, function(all) {
-                var args = [].slice.call(arguments),
-                    offset = args[args.length - 2],
-                    newTextNode = child.splitText(offset+bk), tag;
-                bk -= child.data.length + all.length;
-
-                newTextNode.data = newTextNode.data.substr(all.length);
-                tag = callback.apply(window, [child].concat(args));
-                child.parentNode.insertBefore(tag, newTextNode);
-                child = newTextNode;
-            });
-            break;
-        }
-        child = child.nextSibling;
-    }
-
-    return node;
-};
+      return node;
+  };
+}
--- popup.js
+++ popup.js
@@ -1,21 +1,9 @@
 window.onload = function() {
-  var x = document.getElementById('myhighlight')
-  console.log(x);
-  if (x === null) {
-    throw 812;
-  }
+  var x = document.getElementById('submit_button')
+
   x.addEventListener('click', sendHighlightMessage);
-  document.addEventListener('DOMContentLoaded', function() {
-    throw 3903;
-    var link = document.getElementById('highlight');
-    // onClick's logic below:
-    link.addEventListener('click', function() {
-      sendHighlightMessage();
-    });
-  });
 
   function sendHighlightMessage() {
-    // This sentence has been reached
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {highlight: true}, function(response) {
         console.log(response);
