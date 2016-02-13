dbdcce6f370ccc4c5e6dcf707442dbcf1aad746e
Devin Lange <lange604@umn.edu>
2016-02-13 16:30:35 -0600

    highlight working

--- content_script.js
+++ content_script.js
@@ -1,42 +1,71 @@
 //document.body.style.background = 'blue'
 
-window.onload = function() {
-  function makeEditableAndHighlight(colour) {
-      var range, sel = window.getSelection();
-      if (sel.rangeCount && sel.getRangeAt) {
-          range = sel.getRangeAt(0);
-      }
-      document.designMode = "on";
-      if (range) {
-          sel.removeAllRanges();
-          sel.addRange(range);
-      }
-      // Use HiliteColor since some browsers apply BackColor to the whole block
-      if (!document.execCommand("HiliteColor", false, colour)) {
-          document.execCommand("BackColor", false, colour);
-      }
-      document.designMode = "off";
-  }
+/*
+ * Hide
+ * The following is commented out temporarily
+ */
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  // we reached here
+//  inputText = document.getElementById("inputText")
+//  var text = "ab";
+//  var inputText = document.body;
+//  var innerHTML = inputText.innerHTML;
+//  var index = innerHTML.indexOf(text);
+//  console.log("hello this is Hidenori");
+//  console.log("index is");
+//  console.log(index);
+//  if ( index >= 0 )
+//  { 
+//      innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+//      inputText.innerHTML = innerHTML 
+//  }
 
-  function highlight(colour) {
-      var range, sel;
-      if (window.getSelection) {
-          // IE9 and non-IE
-          try {
-              if (!document.execCommand("BackColor", false, colour)) {
-                  makeEditableAndHighlight(colour);
-              }
-          } catch (ex) {
-              makeEditableAndHighlight(colour)
-          }
-      } else if (document.selection && document.selection.createRange) {
-          // IE <= 8 case
-          range = document.selection.createRange();
-          range.execCommand("BackColor", false, colour);
-      }
-  }
-
-  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    highlight('yellow');
+  if (true) {
+    // highlightText(document.body);
+    var searchTerm = "Example";
+;
+  matchText(document, new RegExp("\\b" + searchTerm + "\\b", "g"), function(node, match, offset) {
+      var span = document.createElement("span");
+      span.style = "background-color:#FFFF01";
+      span.textContent = match;
+      return span;
   });
-}
+
+
+    sendResponse({messageStatus: "received"});
+  }
+});
+
+
+var matchText = function(node, regex, callback, excludeElements) { 
+
+    excludeElements || (excludeElements = ['script', 'style', 'iframe', 'canvas']);
+    var child = node.firstChild;
+
+    while (child) {
+        switch (child.nodeType) {
+        case 1:
+            if (excludeElements.indexOf(child.tagName.toLowerCase()) > -1)
+                break;
+            matchText(child, regex, callback, excludeElements);
+            break;
+        case 3:
+            var bk = 0;
+            child.data.replace(regex, function(all) {
+                var args = [].slice.call(arguments),
+                    offset = args[args.length - 2],
+                    newTextNode = child.splitText(offset+bk), tag;
+                bk -= child.data.length + all.length;
+
+                newTextNode.data = newTextNode.data.substr(all.length);
+                tag = callback.apply(window, [child].concat(args));
+                child.parentNode.insertBefore(tag, newTextNode);
+                child = newTextNode;
+            });
+            break;
+        }
+        child = child.nextSibling;
+    }
+
+    return node;
+};
