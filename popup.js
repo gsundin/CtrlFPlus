6c6783328fbc8a2d33d4eb9a1ebf7cd2a15669e0
Hidenori Shinohara <shino013@umn.edu>
2016-02-13 22:33:10 -0600

    fix some bugs

--- popup.js
+++ popup.js
@@ -1,6 +1,5 @@
 window.onload = function() {
 
-  document.getElementById('submit_button').addEventListener('click', sendHighlightMessage);
   var textInput = document.getElementById('input_text');
 
   textInput.focus();
